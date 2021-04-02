d31a5c1a96dae7e8bbc0b540a7684bc96d050c26
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-03 01:34:13 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {} from '../public/background.jpg';
+import React from "react";
+
+export class App extends React.Component{
+    render() {
+        return (
+            <div className="App">
+                Hello World
+            </div>
+        );
+    }
+}
+
