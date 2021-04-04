1fca9b993ff5fdfdb43cf7b9de8c328471397c1b
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-04 13:50:58 +0530

    Login Page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import './App.css';
 import React from "react";
 import {FirstPage} from "./Views/FirstPage/FirstPage";
 import {Footer} from "./Views/Footer/Footer";
+import {BrowserRouter, Route} from "react-router-dom";
+import {LoginPage} from "./Views/LoginPage/LoginPage";
 
 export class App extends React.Component{
     render() {
         return (
+            <BrowserRouter>
             <div className="App">
-                <FirstPage/>
+                {/*<FirstPage/>*/}
+
+                {/*<BrowserRouter>*/}
+                    <Route path='/' exact component={FirstPage}/>
+                    <Route path='/login' exact component={LoginPage}/>
+                {/*</BrowserRouter>*/}
                 <Footer />
             </div>
+            </BrowserRouter>
         );
     }
 }
--- src/Views/FirstPage/FirstPage.tsx
+++ src/Views/FirstPage/FirstPage.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import logo from '../../logo.svg'
 import './FirstPage.css'
 import image from '../../assets/images/frontpage.svg'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 export class FirstPage extends React.Component{
     render(){
@@ -36,7 +43,7 @@ export class FirstPage extends React.Component{
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                               d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                     </svg>
-                                    Log In
+                                    <Link to='/login'>Log In</Link>
                                 </button>
                             </div>
                         </div>
