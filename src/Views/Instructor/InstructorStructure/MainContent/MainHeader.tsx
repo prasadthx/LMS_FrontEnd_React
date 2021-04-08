6d703ac60df3715053724a76b9bd94c3dda1543c
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-08 17:23:49 +0530

    React Icons package added

--- src/Views/Instructor/InstructorStructure/MainContent/MainHeader.tsx
+++ src/Views/Instructor/InstructorStructure/MainContent/MainHeader.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import {useState} from "react";
 import ReactDOM from 'react-dom'
 import './MainHeader.css';
+import {FiMenu} from 'react-icons/fi'
 
 export class MainHeader extends React.Component<any,any> {
     render() {
         return (
             <div className='MainHeader'>
                 <div className='DnyaanHeader text-center flex justify-between'>
-                    <div><img src="https://img.icons8.com/ios/24/000000/menu--v1.png" /></div>
+                    <div className='text-white text-2xl pl-2 pt-0.5'><FiMenu onClick={this.props.toggleSidebar}/></div>
                     <div>
                         Dnyaan
-                        <button onClick={this.props.toggleSidebar}>
-                            Toggle
-                        </button>
                     </div>
                     <div> Hello User </div>
                 </div>
